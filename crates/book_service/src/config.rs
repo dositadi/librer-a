ef96a946f209b12815de5a030d3ae0452180641b
dositadi <akindivine587@gmail.com>
2026-09-15 16:59:53 +0100

    added the config file

---
+++ crates/book_service/src/config.rs
@@ -0,0 +1,37 @@
+use envconfig::Envconfig;
+
+#[derive(Envconfig)]
+pub struct AppConf {
+    #[envconfig(nested)]
+    pub server: ServerConf,
+}
+
+impl AppConf {
+    pub fn init() -> Self {
+        Self::init_from_env().expect("Failed to load configuration!, check the .env file.")
+    }
+}
+
+#[derive(Envconfig)]
+pub struct ServerConf {
+    #[envconfig(from = "SERVER_PORT")]
+    pub port: u16,
+
+    #[envconfig(from = "SERVER_ALLOWED_METHODS")]
+    pub allowed_methods: String,
+
+    #[envconfig(from = "SERVER_ALLOWED_ORIGINS")]
+    pub allowed_origins: String,
+
+    #[envconfig(from = "SERVER_ALLOWED_HEADERS")]
+    pub allowed_headers: String,
+
+    #[envconfig(from = "SERVER_DEFAULT_BODY_LIMIT")]
+    pub default_body_limits: usize,
+}
+
+impl ServerConf {
+    pub fn to_addr(&self) -> String {
+        format!("0.0.0.0:{}", self.port)
+    }
+}
