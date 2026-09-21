64bf325338ba6d1c296fee29cca4f22152e64747
dositadi <akindivine587@gmail.com>
2026-09-21 17:05:17 +0100

    still learning

--- crates/book_service/src/bin/app.rs
+++ crates/book_service/src/bin/app.rs
@@ -1,4 +1,4 @@
-use book_service::{ AppConf, AppState, routes };
+use book_service::{ AppConf, AppState, models::Book, routes };
 use tokio::net::TcpListener;
 use tracing::info;
 use tracing_subscriber::{ EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt };
@@ -16,9 +16,17 @@ async fn main() -> anyhow::Result<()> {
         .init();
 
     let conf = AppConf::init();
+
+    let db = toasty::Db
+        ::builder()
+        .models(toasty::models!(Book))
+        .connect(&conf.db.into_db_url()).await
+        .expect("Failed to connect to database");
+
     let addr = conf.server.to_addr();
     let server_conf = conf.server;
-    let app_state = AppState { server_conf };
+
+    let app_state = AppState { db, server_conf };
 
     let app = routes::init(app_state);
     info!(addr=%addr, "Starting server");
